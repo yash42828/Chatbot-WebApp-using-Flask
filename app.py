5fa5676602b9faa4f25c69ff9ec47d5da8155670
yash42828 <bajajyash42828@gmail.com>
2020-04-29 15:49:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from chatbot import chatbot
-import win32com.client
 from flask import Flask, render_template, request
-#speaker = win32com.client.Dispatch("SAPI.SpVoice")
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,12 +9,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
-    #speaker.Speak(userText)    
     resp = str(chatbot.get_response(userText))
-    #print(resp)
-    #tts.Speak("Hello, fellow Python programmer")
-    #speaker.Speak("Hello, fellow Python programmer")    
-    #speak(resp)
     return (resp)
 
 if __name__ == "__main__":
